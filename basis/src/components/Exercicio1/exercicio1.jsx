d8b663c9d4c1f0daec53a1a33e5fd642b8f28bb9
Jonh Sousa <jonhnbsousa@gmail.comh>
2022-05-16 21:39:59 -0300

    feat - Exercicio1 component created and used on index.jsx (components folder being used)

---
+++ basis/src/components/Exercicio1/exercicio1.jsx
@@ -0,0 +1,9 @@
+import styles from './Exercicio1.module.css'
+export default function Exercicio1() {
+  return (
+    <div className={styles.vermelho}>
+      <h1>Exercício 1</h1>
+      <p>Esse é o conteúdo do exercício 1</p>
+    </div>
+  )
+}
--- basis/src/pages/index.jsx
+++ basis/src/pages/index.jsx
@@ -1,3 +1,10 @@
+import Exercicio1 from "../components/Exercicio1/exercicio1"
 export default function Inicio() {
-  return "Inicio"
+  return (
+      <div>
+        <h1>Título</h1>
+        <p>Substítulo</p>
+        <Exercicio1></Exercicio1>
+      </div>
+  )
 }
