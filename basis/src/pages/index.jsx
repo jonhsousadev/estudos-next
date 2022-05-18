ead9daa8612ebf41a8c66f2fdce2ccb33b4d5456
Jonh Sousa <jonhnbsousa@gmail.comh>
2022-05-18 20:33:31 -0300

    feat- cadastro simples initial commit

--- basis/src/pages/index.jsx
+++ basis/src/pages/index.jsx
@@ -1,11 +1,12 @@
 import Cabecalho from "../components/cabecalho/Cabecalho"
 import Navegador from "../components/navegador/Navegador"
+import Layout from "../components/layout/Layout"
 export default function Inicio() {
   let title = "Esse é o nosso cabeçalho" 
   return (
-      <>
+      <Layout titulo="Página Inicial">
         <Cabecalho title={title}/>
         <Navegador path="/navecacao1" label="Navegacao 1" color="darkcyan"/>
-      </>
+      </Layout>
   )
 }
--- basis/src/pages/navecacao1.jsx
+++ basis/src/pages/navecacao1.jsx
@@ -3,7 +3,7 @@ import Layout from '../components/layout/Layout'
 export default function Navecacao1() {
   return (
     <>
-      <Layout>
+      <Layout titulo="Navegação 1">
         <h1>Nova página de navegação</h1>
       </Layout>
     </>
