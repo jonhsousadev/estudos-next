ae289f0a286f38dbb8cd290282d3e6070d5059b8
Jonh Sousa <jonhnbsousa@gmail.comh>
2022-05-18 22:26:06 -0300

    feat- components: Layout and Titulo using tailwind css classes

---
+++ cadastro-simples/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout/Layout";
+
+export default function Home() {
+  return (
+    <div className="h-screen flex justify-center items-center bg-gradient-to-r from-purple-500 to to-blue-600">
+      <Layout titulo="Cadastro Simples">
+        <span>Conteúdo</span>
+      </Layout>
+    </div>
+  )
+}
